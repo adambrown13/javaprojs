1f4c9698716f7929694ccfc8c4e0b88604b9ee4e
Adam Brown <adambrown@berkeley.edu>
2017-03-14 20:19:14 -0700

    tail length extension and food added

--- Snakes/Field.java
+++ Snakes/Field.java
@@ -1,12 +1,17 @@
+import java.util.Random;
+
 public class Field{
 
     int[] head;
     int[][] tail;
     Snake fieldSnake; 
+    int[] food;
+    int score = 0;
 
     public Field(){
 
         fieldSnake = new Snake();
+        food = new int[] {19, 25};
 
     }
 
@@ -18,6 +23,23 @@ public class Field{
         StdDrawPlus.square(16.5, 16.5, 16.5);
         StdDrawPlus.setPenRadius();
         
+    }
+
+    private void drawFood(){
+        StdDrawPlus.setPenColor(StdDrawPlus.BLUE);
+        StdDrawPlus.filledSquare(food[0], food[1], 0.5);
+        
+    }
+
+    private void foodFound() {
+        Random rand = new Random();
+
+        if (head[0] == food[0] && head[1] == food[1]) {
+            fieldSnake.foodCollected();
+            score += 1;
+            food[0] = rand.nextInt(31) + 1;
+            food[1] = rand.nextInt(31) + 1;
+        }
     }
 
     private void drawSnake(){
@@ -36,6 +58,18 @@ public class Field{
         fieldSnake.down();
     }
 
+    private void snakeUp(){
+        fieldSnake.up();
+    }
+
+    private void snakeLeft(){
+        fieldSnake.left();
+    }
+
+    private void snakeRight(){
+        fieldSnake.right();
+    }
+
     private boolean snakeAlive() {
         if (head == null) {
             return true;
@@ -43,6 +77,9 @@ public class Field{
         if (head[0] == 0 || head[0] == 33 || head[1] == 0 || head[1] == 33) {
             return false;
         }
+        /*if (tail.contains(head)) {
+            return
+        }*/
         return true;
     }
 
@@ -50,11 +87,55 @@ public class Field{
         StdDrawPlus.setXscale(0, 33);
         StdDrawPlus.setYscale(0, 33);
         Field test = new Field();
-        
+        int i = 0;
+        // direction key: 1 = left, 2 = up, 3 = right, 4 = down 
+        int direction = 4;
         while(test.snakeAlive()) {
             test.drawBoard();
             test.drawSnake();
-            test.snakeDown();
+            test.foodFound();
+            test.drawFood();
+            while (i < 20000000/((test.score + 1) * 4)) {
+                i += 1;
+            }
+            if(StdDrawPlus.isKeyPressed(37)){
+                //Left Key has been pressed
+                if (direction%2 == 0) {
+                    direction = 1;
+                }
+            }
+            if(StdDrawPlus.isKeyPressed(38)){
+                //Up Key has been pressed
+                if (direction%2 == 1) {
+                    direction = 2;
+                }
+            }
+            if(StdDrawPlus.isKeyPressed(39)){
+                //Right Key has been pressed
+                if (direction%2 == 0) {
+                    direction = 3;
+                }
+            }
+            if(StdDrawPlus.isKeyPressed(40)){
+                //Down Key has been pressed
+                if (direction%2 == 1) {
+                    direction = 4;
+                }
+            }
+            if (direction == 1) {
+                test.snakeLeft();
+            }
+            if (direction == 2) {
+                test.snakeUp();
+            }
+            if (direction == 3) {
+                test.snakeRight();
+            }
+            if (direction == 4) {
+                test.snakeDown();
+            }
+            i = 0;
+            
 
         }
         
--- Snakes/Snake.java
+++ Snakes/Snake.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 
 public class Snake {
+
     int size;
     ArrayList<int[]> tail;
     int[] head = {0,0};
+    int tailAdd = 0;
+
 
     public Snake(){
         tail = new ArrayList<int[]>();
@@ -17,11 +20,56 @@ public class Snake {
     }
 
     public void down() {
-        tail.remove(0);
+        if (tailAdd != 0) {
+            tailAdd -= 1;
+        } else {
+            tail.remove(0);
+        }
         tail.add(new int[] {head[0], head[1]});
         int temp = head[1];
         head[1] = temp - 1;
 
+    }
+
+    public void left() {
+        if (tailAdd != 0) {
+            tailAdd -= 1;
+        } else {
+            tail.remove(0);
+        }
+        tail.add(new int[] {head[0], head[1]});
+        int temp = head[0];
+        head[0] = temp - 1;
+
+    }
+
+    public void up() {
+        if (tailAdd != 0) {
+            tailAdd -= 1;
+        } else {
+            tail.remove(0);
+        }
+        tail.add(new int[] {head[0], head[1]});
+        int temp = head[1];
+        head[1] = temp + 1;
+
+    }
+
+    public void right() {
+        if (tailAdd != 0) {
+            tailAdd -= 1;
+        } else {
+            tail.remove(0);
+        }
+        tail.add(new int[] {head[0], head[1]});
+        int temp = head[0];
+        head[0] = temp + 1;
+
+    }
+
+    public void foodCollected() {
+        tailAdd = 2;
+        return;
     }
 
     public int[] returnHead() {
